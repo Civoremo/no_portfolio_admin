688f39bd0341325fd8a292520806e42a5b032a9b
Civoremo <civoremon@gmail.com>
2021-07-26 13:23:16 -0400

    about - edit, delete buttons and styling

--- portfolio-admin/src/components/About.js
+++ portfolio-admin/src/components/About.js
@@ -6,6 +6,16 @@ import React, { useState, useEffect } from "react";
 const About = () => {
   const [aboutContent, setAboutContent] = useState(null);
 
+  const editContent = (e, sectionId) => {
+    e.preventDefault();
+    console.log("edit button clicked", sectionId);
+  };
+
+  const deleteContent = (e, sectionId) => {
+    e.preventDefault();
+    console.log("delete button clicked", sectionId);
+  };
+
   useEffect(() => {
     axios({
       method: "get",
@@ -27,12 +37,58 @@ const About = () => {
       //   console.log("section", section);
       return (
         <div key={section[0].id}>
-          <div>{section[0].title}</div>
+          <div
+            style={{
+              display: "flex",
+              // border: "1px solid red",
+              width: "80px",
+              justifyContent: "space-between",
+            }}
+          >
+            <div
+              className='content-interaction-buttons'
+              onClick={event => editContent(event, section[0].id)}
+            >
+              Edit
+            </div>
+            <div
+              className='content-interaction-buttons'
+              onClick={event => deleteContent(event, section[0].id)}
+            >
+              Delete
+            </div>
+          </div>
+          <h2
+            style={{
+              textAlign: "left",
+              margin: "0 70px 30px 70px",
+              fontFamily: "Nunito",
+              color: "#DC3545",
+            }}
+          >
+            {section[0].title}
+          </h2>
           <br />
           <div>
             {section[1].map(content => {
               //   console.log("content", content);
-              return <div key={content.id}>{content.textContent}</div>;
+              return (
+                <div
+                  key={content.id}
+                  style={{
+                    display: "flex",
+                    margin: "0 100px 0 100px",
+                  }}
+                >
+                  <div
+                    style={{
+                      margin: "0 0 20px 50px",
+                    }}
+                  >
+                    {content.textContent}
+                  </div>
+                </div>
+              );
             })}
           </div>
           <br />
