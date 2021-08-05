999156b00ad317541491ec1364103f5ceadfda53
Civoremo <civoremon@gmail.com>
2021-08-05 13:30:42 -0400

    project main info update, info set to null functionality

--- portfolio-admin/src/components/EditProject.js
+++ portfolio-admin/src/components/EditProject.js
@@ -13,6 +13,7 @@ const EditProject = () => {
   const [frontendInput, setFrontendInput] = useState("");
   const [backendInput, setBackendInput] = useState("");
   const [projectInfo, setProjectInfo] = useState(null);
+  const [changeCount, setChangeCount] = useState(0);
 
   useEffect(() => {
     axios({
@@ -36,7 +37,7 @@ const EditProject = () => {
         // console.log("something went wrong with fetching projects");
         alert("Failed to fetch Project data!");
       });
-  }, [id]);
+  }, [id, changeCount]);
 
   const displayProjectData = () => {
     return (
@@ -105,6 +106,7 @@ const EditProject = () => {
               }
               onChange={event => setStackInput(event.target.value)}
             />
+            <button onClick={event => removeData(event, "stack")}>Clean</button>
             <br />
             <label>Live Link</label>
             <input
@@ -123,6 +125,9 @@ const EditProject = () => {
               }
               onChange={event => setLiveLinkInput(event.target.value)}
             />
+            <button onClick={event => removeData(event, "liveLink")}>
+              Clean
+            </button>
             <label>Frontend Link</label>
             <input
               type='text'
@@ -140,10 +145,13 @@ const EditProject = () => {
               }
               onChange={event => setFrontendInput(event.target.value)}
             />
+            <button onClick={event => removeData(event, "frontendLink")}>
+              Clean
+            </button>
             <label>Backend Link</label>
             <input
               type='text'
-              name='Project Backend Link'
+              name='backendLink'
               placeholder={
                 projectInfo[0].backendLink === null
                   ? "Backend URL ..."
@@ -157,10 +165,15 @@ const EditProject = () => {
               }
               onChange={event => setBackendInput(event.target.value)}
             />
+            <button onClick={event => removeData(event, "backendLink")}>
+              Clean
+            </button>
           </form>
           <br />
           <div>
-            <button>Save Changes</button>
+            <button onClick={event => saveProjectData(event)}>
+              Save Changes
+            </button>
             <button onClick={event => resetInputFields(event)}>Reset</button>
           </div>
         </div>
@@ -176,6 +189,116 @@ const EditProject = () => {
     setLiveLinkInput("");
     setFrontendInput("");
     setBackendInput("");
+  };
+
+  const setNewData = () => {
+    let updatedData = {
+      id: id,
+      title: titleInput === "" ? projectInfo[0].title : titleInput,
+      description:
+        descriptionInput === "" ? projectInfo[0].title : descriptionInput,
+      stack:
+        // if input is empty and project stack data is null
+        stackInput === "" && projectInfo[0].stack === null
+          ? // leave it as null
+            null
+          : //otherwise, if input is empty but project stack data has info
+          stackInput === "" && projectInfo[0].stack !== null
+          ? // leave it as the data it already has
+            projectInfo[0].stack
+          : // otherwise, set project stack data as the new input text
+            stackInput,
+      liveLink:
+        // input is empty and data is null
+        liveLinkInput === "" && projectInfo[0].liveLink === null
+          ? // leave as null
+            null
+          : // if input is empty but data has info
+          liveLinkInput === "" && projectInfo[0].liveLink !== null
+          ? // leave exsisting data
+            projectInfo[0].liveLink
+          : // otherwise set new input data
+            liveLinkInput,
+      frontendLink:
+        // input is empty and data is null
+        frontendInput === "" && projectInfo[0].frontendLink === null
+          ? // leave as null
+            null
+          : // if input is empty but data has info
+          frontendInput === "" && projectInfo[0].frontendLink !== null
+          ? // leave existing data
+            projectInfo[0].frontendLink
+          : // otherwise set new input data
+            frontendInput,
+      backendLink:
+        // input is empty and data is null
+        backendInput === "" && projectInfo[0].backendLink === null
+          ? // leave as null
+            null
+          : // if input is empty but data has info
+          backendInput === "" && projectInfo[0].backendLink !== null
+          ? // leave existing data
+            projectInfo[0].backendLink
+          : // otherwise set new input data
+            backendInput,
+    };
+
+    return updatedData;
+  };
+
+  const saveProjectData = e => {
+    e.preventDefault();
+
+    axios({
+      method: "put",
+      url: `${process.env.REACT_APP_API_URL}/projects/update`,
+      headers: {
+        authorization: JSON.parse(localStorage.getItem("on_portfolio_token")),
+      },
+      data: setNewData(),
+      responseType: "json",
+    })
+      .then(result => {
+        // console.log("update project info", result);
+        if (result.status === 200) {
+          setChangeCount(changeCount + 1);
+        } else {
+          alert("Something went wrong with update of project info.");
+        }
+      })
+      .catch(err => {
+        // console.log("Failed to update project info.");
+        alert("Failed to update project info!");
+      });
+  };
+
+  const removeData = (e, name) => {
+    e.preventDefault();
+
+    axios({
+      method: "put",
+      url: `${process.env.REACT_APP_API_URL}/projects/update`,
+      headers: {
+        authorization: JSON.parse(localStorage.getItem("on_portfolio_token")),
+      },
+      data: {
+        id: id,
+        [name]: null,
+      },
+      responseType: "json",
+    })
+      .then(result => {
+        // console.log("set data to null", result);
+        if (result.status === 200) {
+          setChangeCount(changeCount + 1);
+        } else {
+          alert("Something went wrong with removing data!");
+        }
+      })
+      .catch(err => {
+        // console.log("failed to set data to null");
+        alert("Failed to set data to null!");
+      });
   };
 
   if (projectInfo === null) {
