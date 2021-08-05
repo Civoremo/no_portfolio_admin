9f23815c49a59b42234c80a7ce2dd063baaf053a
Civoremo <civoremon@gmail.com>
2021-08-05 17:14:53 -0400

    upload carousel image for extended project info

--- portfolio-admin/src/components/EditProjectExtendedInfo.js
+++ portfolio-admin/src/components/EditProjectExtendedInfo.js
@@ -82,8 +82,93 @@ const EditProjectExtendedInfo = () => {
         </form>
         <button>Save</button>
         <button>Reset</button>
+        <br />
+        <hr />
+        <br />
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
+          {projectInfo[1].map(image => {
+            return (
+              <div
+                key={image.id}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}
+              >
+                <img
+                  style={{
+                    width: "auto",
+                    height: "200px",
+                    margin: "20px 20px 0 20px",
+                  }}
+                  src={image.image}
+                  alt={projectInfo[1].id + " carousel image"}
+                />
+                <button style={{ width: "200px" }}>Delete</button>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
+  };
+
+  const uploadImage = (e, files) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("file", files[0]);
+    formData.append("upload_preset", process.env.REACT_APP_UPLOAD_IMAGE_PRESET);
+
+    const config = {
+      headers: { "X-Requested-With": "XMLHttpRequest" },
+    };
+
+    axios
+      .post(
+        `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_IMAGE_CLOUD_NAME}/image/upload`,
+        formData,
+        config
+      )
+      .then(result => {
+        // console.log("image uploaded", result);
+        if (result.status === 200) {
+          postCrouselImage(result.data.secure_url);
+        } else {
+          alert("Something went wrong with uploading image to cloudinary!");
+        }
+      })
+      .catch(err => {
+        // console.log("failed to upload image");
+        alert("Failed to upload to Cloudinary!");
+      });
+  };
+
+  const postCrouselImage = imageURL => {
+    axios({
+      method: "post",
+      url: `${process.env.REACT_APP_API_URL}/projects/image/register`,
+      headers: {
+        authorization: JSON.parse(localStorage.getItem("on_portfolio_token")),
+      },
+      data: {
+        project_id: id,
+        image: imageURL,
+      },
+      responseType: "json",
+    })
+      .then(result => {
+        // console.log("gif image updated");
+        if (result.status === 201) {
+          setChangeCount(changeCount + 1);
+        } else {
+          alert("Something went wrong with updating image for project.");
+        }
+      })
+      .catch(err => {
+        alert("Failed to update gif image for project!");
+      });
   };
 
   if (projectInfo === null) {
@@ -92,7 +177,17 @@ const EditProjectExtendedInfo = () => {
 
   return (
     <div>
-      <div>Edit Extended Project Info Component</div>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <label>Upload Image for Carousel</label>
+        <br />
+        <input
+          type='file'
+          multiple={false}
+          onChange={event => uploadImage(event, event.target.files)}
+        />
+      </div>
+      <br />
+      <hr />
       <div>{displayProjectInfo()}</div>
     </div>
   );
